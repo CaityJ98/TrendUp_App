9d15354373f78e382e35b67e6c80156df4f0d3ee
CaityJ98 <caitlynjschaper@gmail.com>
2022-03-17 11:17:26 +1100

    additions to categories and listing models

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,11 @@
 class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  #number mapping to match features to a value
+
+  # conditions A Bit worn, Worn a few times, Like New , Brand New 
+  # {"a bit worn" => 1, "worn a few times" => 2, "like new" => 3, "Brand New" => 4}
+  enum condition: {a_bit_worn: 1, worn_a_few_times: 2, like_new: 3, brand_new: 4}
+
+
 end
