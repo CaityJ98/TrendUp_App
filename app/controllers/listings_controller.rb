8b8d57c695cddef84f705b9be4f8bd238f9dd417
CaityJ98 <caitlynjschaper@gmail.com>
2022-03-17 17:00:37 +1100

    added create and update listings functionality

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,12 +1,13 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
   def index
     @listings = Listing.all
   end
 
   def show
-    @listing = Listing.find(params[:id])
+  
   end
 
   def new
@@ -14,23 +15,49 @@ class ListingsController < ApplicationController
   end
 
   def create
+    @listing = current_user.listings.new(listing_params) 
+    if @listing.save
+      redirect_to @listing, notice: "Listing successfuly created"
+    else
+      pp @listing.errors
+      set_form_vars
+      render "new", notice:"something went wrong"
 
   end
+end 
 
   def edit
 
   end
 
   def update
+    @listing.update(listing_params) 
+    if @listing.save
+      redirect_to @listing, notice: "Listing successfuly updated"
+    else
+      pp @listing.errors
+      set_form_vars
+      render "edit", notice:"something went wrong"
 
+  end
   end 
 
   def destroy
+    @listing.destroy
 
   end 
 
   private
-  def set_listing
 
+  def listing_params
+    params.require(:listing).permit(:title, :price, :category_id, :condition, :description)
+  end
+  def set_listing
+    @listing = Listing.find(params[:id])
   end 
+
+  def set_form_vars
+    @categories = Category.all
+    @conditions = Listing.conditions.keys
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'restricted', to: 'pages#restricted'
   get 'listings', to: 'listings#index', as: 'listings'
-  post "listings", to: "listing#create" 
+  post "listings", to: "listings#create" 
   get "listings/new", to: "listings#new", as: "new_listing" 
   get 'listings/:id', to: 'listings#show', as: 'listing'
+  get "listings/:id", to: "listings#update" 
+  patch "listings/:id", to: "listings#update"
+  delete "listings/:id", to: "listings#destroy"
+  get "listings/:id/edit", to: 'listings#edit', as: "edit_listing"
+
+  
 end
