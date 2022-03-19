42c1aba7a5463f74e97c2dcbbf6fa4cb238c23c3
CaityJ98 <caitlynjschaper@gmail.com>
2022-03-19 16:04:35 +1100

    implemented basic image storage and background image

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,6 +2,9 @@ class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
   before_action :set_form_vars, only: [:new, :edit]
+  
+
+
   def index
     @listings = Listing.all
   end
@@ -50,14 +53,16 @@ end
   private
 
   def listing_params
-    params.require(:listing).permit(:title, :price, :category_id, :condition, :description)
+    params.require(:listing).permit(:title, :price, :category_id, :condition, :description, :picture)
   end
   def set_listing
     @listing = Listing.find(params[:id])
   end 
 
   def set_form_vars
+ 
     @categories = Category.all
     @conditions = Listing.conditions.keys
   end
+  
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -6,6 +6,7 @@ class Listing < ApplicationRecord
   # conditions A Bit worn, Worn a few times, Like New , Brand New 
   # {"a bit worn" => 1, "worn a few times" => 2, "like new" => 3, "Brand New" => 4}
   enum condition: {a_bit_worn: 1, worn_a_few_times: 2, like_new: 3, brand_new: 4}
-
+# enabled image upload in create listing image upload attribute
+  has_one_attached :picture
 
 end
